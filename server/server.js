c6830c857b960eba2904e1df9d665a83af3ff1b7
Aditya Rai <aditya.rai7297@gmail.com>
2023-08-12 13:40:00 -0700

    Server setup

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+// Connect to the RDS PostgreSQL instance
+const pool = new Pool({
+  host: 'YOUR_RDS_ENDPOINT',
+  user: 'YOUR_DB_USERNAME',
+  password: 'YOUR_DB_PASSWORD',
+  database: 'YOUR_DB_NAME',
+  port: 5432,
+  ssl: { rejectUnauthorized: false }  // For demo purposes only
+});
+
+app.use(bodyParser.json());
+
+// Sample endpoint to add a workout
+app.post('/addWorkout', async (req, res) => {
+  try {
+    const workout = req.body;
+
+    // Basic SQL query to insert workout into a table
+    const query = 'INSERT INTO workouts(name, sets) VALUES($1, $2)';
+    const values = [workout.name, workout.sets];
+
+    await pool.query(query, values);
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
